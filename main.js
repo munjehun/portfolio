abbdf443cac13f46069abaf3df6a89651b145cb0
munjehun <miso1489@gmail.com>
2022-12-19 23:53:03 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// 네비바 투명-불투명화
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
